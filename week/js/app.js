c5ab08dd42b06c9fef05cdfe126acf3750670ed4
FirdavsEminov FE[14] <114588905+Firdavs-owafr@users.noreply.github.com>
2022-10-09 09:15:40 -0600

    Add files via upload

---
+++ week/js/app.js
@@ -0,0 +1,26 @@
+let a = prompt("hafta kunlari")
+switch (a){
+    case '1':
+        console.log('Monday');
+        break;
+        case '2':
+            console.log('Tuesdqay');
+            break;
+            case '3':
+            console.log('Wensday');
+            break;
+            case '4':
+            console.log('Thursday');
+            break;
+            case '5':
+            console.log('Friday');
+            break;
+            case '6':
+            console.log('Saturday');
+            break;
+            case '7':
+            console.log('Sunday');
+            break;
+            default:
+                console.log('unday hafta mavjud emas');
+}
